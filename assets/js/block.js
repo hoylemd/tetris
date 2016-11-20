cab9df27a8748162fb4eb6a32705de20dab63eac
Michael D. Hoyle <hoylemd@gmail.com>
2016-11-20 12:42:20 -0500

    implement block drawing

--- assets/js/block.js
+++ assets/js/block.js
@@ -4,6 +4,15 @@ var GridElement = require('./grid_element.js');
 
 function Block(column, row, colour, border_colour) {
   GridElement.call(this, column, row);
+
+  this.colour = colour;
+  this.border_colour = colour;
+
+  var primitives = new PIXI.Graphics();
+  primitives.beginFill(colour);
+  primitives.lineStyle(2, border_colour, 1);
+
+  primitives.drawRect(0, 0, GridElement.WIDTH, GridElement.HEIGHT);
 }
 Block.prototype = GridElement(0, 0);
 
