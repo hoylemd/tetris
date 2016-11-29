3728af6db089670ddf9cd1b57bd7f810c62f7dec
Michael D. Hoyle <hoylemd@gmail.com>
2016-11-29 08:46:14 -0500

    Add blocks and block matrix to block spaces

--- assets/js/block_space.js
+++ assets/js/block_space.js
@@ -8,6 +8,13 @@ function BlockSpace(column, row, columns, rows, seed, time_to_fall) {
   this.seed = seed;
   this.time_to_fall = time_to_fall || null;
 
+  this.blocks = [];
+  this.block_matrix = [];
+  for (var i = 0; i < columns; i += 1) {
+    this.block_matrix.push([]);
+    this.block_matrix[i][rows] = null;
+  }
+
   this.add_tetromino = function BlockSpace_add_tetromino(tetromino) {
     this.addChild(tetromino);
     tetromino.updatePosition(this.seed.column, this.seed.row);
