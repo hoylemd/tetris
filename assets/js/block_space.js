9d1c0c3c0db8e751b339e0459cd59b0e4a0cdd6e
Michael D. Hoyle <hoylemd@gmail.com>
2016-11-29 08:58:41 -0500

    add add and remove block methods

--- assets/js/block_space.js
+++ assets/js/block_space.js
@@ -21,6 +21,29 @@ function BlockSpace(column, row, columns, rows, seed, time_to_fall) {
     this.addChild(tetromino);
     tetromino.updatePosition(this.seed.column, this.seed.row);
   };
+
+  this.add_block = function BlockSpace_add_block(block, column, row) {
+    block.column = column;
+    block.row = row;
+    this.children.push(block);
+    this.block_matrix[column][row] = block;
+  };
+
+  this.remove_block = function BlockSpace_remove_block(block_or_column, row) {
+    var block = null;
+    var column = null;
+    if ((row || row === 0) && is_numeric(block_or_column)) {
+      column = block_or_column;
+      block = this.block_matrix[column][row];
+    } else {
+      block = block_or_column;
+      column = block.column;
+      row = block.row;
+    }
+
+    this.children.remove(block);
+    this.block_matrix[column][row] = null;
+  };
 }
 BlockSpace.prototype = new Block(0, 0);
 
